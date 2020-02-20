37a0e8d0c2fa5b530ea516f279efd93cdb2bbcdd
Samuel Serrano Escorcia <41401143+SamuelSerrano@users.noreply.github.com>
2020-02-19 19:27:09 -0500

    Se crea la página Clientes

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/cliente', function () {
+    return view('cliente');
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
